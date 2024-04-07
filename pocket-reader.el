d07e133dc4436c6afad175d83e86eb666e603f6b
Adam Porter <adam@alphapapa.net>
2024-04-07 17:49:29 -0500

    Meta: v0.4-pre

--- pocket-reader.el
+++ pocket-reader.el
@@ -4,7 +4,7 @@
 
 ;; Author: Adam Porter <adam@alphapapa.net>
 ;; Created: 2017-09-25
-;; Version: 0.3
+;; Version: 0.4-pre
 ;; Keywords: pocket
 ;; Package-Requires: ((emacs "25.1") (dash "2.13.0") (kv "0.0.19") (peg "1.0.1") (pocket-lib "0.1") (s "1.10") (ov "1.0.6") (rainbow-identifiers "0.2.2") (org-web-tools "0.1") (ht "2.2"))
 ;; URL: https://github.com/alphapapa/pocket-reader.el
